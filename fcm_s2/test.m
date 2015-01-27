c25db027d219fb2693e0818b2f15250bda601a37
marearth <niezhiwen.nie@gmail.com>
2015-01-27 22:40:06 +0800

    Third commit

---
+++ fcm_s2/test.m
@@ -0,0 +1,42 @@
+%       data = rand(100,2);
+%       [center,U,obj_fcn] = udfcm(data,2);
+%       plot(data(:,1), data(:,2),'o');
+%       hold on;
+%       maxU = max(U);
+%       % Find the data points with highest grade of membership in cluster 1
+%       index1 = find(U(1,:) == maxU);
+%       % Find the data points with highest grade of membership in cluster 2
+%       index2 = find(U(2,:) == maxU);
+%       line(data(index1,1),data(index1,2),'marker','*','color','g');
+%       line(data(index2,1),data(index2,2),'marker','*','color','r');
+%       % Plot the cluster centers
+%       plot([center([1 2],1)],[center([1 2],2)],'*','color','k')
+%       hold off;
+clc;clear
+tic
+d1=imread('wheel.png');
+d2=rgb2gray(d1);
+subplot(1,3,1);
+imshow(d2);
+dnsp=imnoise(d2,'salt & pepper',0.02);
+subplot(1,3,2);
+imshow(dnsp);
+d3=im2double(dnsp);
+d4=reshape(d3,size(d3,1)*size(d3,2),1);
+[center,U,obj_fcn]=udfcm_s2(d3,4,4.2);
+maxU=max(U); 
+index1=find(U(1,:)==maxU);
+index2=find(U(2,:)==maxU);
+index3=find(U(3,:)==maxU);
+index4=find(U(4,:)==maxU);
+r=round(center*255);
+d5=d4;
+d5(index1,1)=r(1);
+d5(index2,1)=r(2);
+d5(index3,1)=r(3);
+d5(index4,1)=r(4);
+d6=reshape(d5,size(d3,1),size(d3,2));
+d7=mat2gray(d6,[0 255]);
+subplot(1,3,3);
+imshow(d7);
+toc
---
+++ fcm_s2/udstepfcm_s2.m
@@ -0,0 +1,33 @@
+function [U_new, center, obj_fcn] = udstepfcm_s2(data,data_mean, U, cluster_n,a,expo)
+%STEPFCM One step in fuzzy c-mean clustering.
+%   [U_NEW, CENTER, ERR] = STEPFCM(DATA, U, CLUSTER_N, EXPO)
+%   performs one iteration of fuzzy c-mean clustering, where
+%
+%   DATA: matrix of data to be clustered. (Each row is a data point.)
+%   U: partition matrix. (U(i,j) is the MF value of data j in cluster j.)
+%   CLUSTER_N: number of clusters.
+%   EXPO: exponent (> 1) for the partition matrix.
+%   U_NEW: new partition matrix.
+%   CENTER: center of clusters. (Each row is a center.)
+%   ERR: objective function for partition U.
+%
+%   Note that the situation of "singularity" (one of the data points is
+%   exactly the same as one of the cluster centers) is not checked.
+%   However, it hardly occurs in practice.
+%
+%       See also DISTFCM, INITFCM, IRISFCM, FCMDEMO, FCM.
+
+%   Roger Jang, 11-22-94.
+%   Copyright 1994-2002 The MathWorks, Inc. 
+
+mf = U.^expo;       % MF matrix after exponential modification
+dm1=reshape(data_mean,size(data_mean,1)*size(data_mean,2),1);
+%dm1=data_mean;
+data1=data+a*dm1;
+center = mf*data1./((ones(size(data1, 2), 1)*sum(mf'))'*(1+a)); % new center
+[t1,t2]=obj_mat(center, data,data_mean,U,expo);
+obj_fcn = sum(sum((t1.^2).*mf))+a*sum(sum(t2));  % objective function
+
+dist = uddistfcm_s2(center, data,dm1,a);     % fill the distance matrix
+tmp = dist.^(-1/(expo-1));      % calculate new U, suppose expo != 1
+U_new = tmp./(ones(cluster_n, 1)*sum(tmp));
